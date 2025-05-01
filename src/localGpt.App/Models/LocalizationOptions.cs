5c08c484e9cd7a77c8a9d46a45257cac3e06f64d
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2025-05-01 13:04:05 +0900

    Remove obsolete localization and application files: delete App.axaml, App.axaml.cs, localization options, and related resources to streamline project structure.

--- src/localGpt.App/Models/LocalizationOptions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace localGpt.Models;
-
-public class LocalizationOptions
-{
-    public string DefaultCulture { get; set; } = "en-US";
-    public string[] SupportedCultures { get; set; } = ["en-US", "ja-JP"];
-}
--- src/localGpt.App/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace localGpt.ViewModels;
-
-public class ViewModelBase : ObservableObject
-{
-}
